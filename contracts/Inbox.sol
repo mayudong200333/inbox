615fd7003b21ee059ea66fc3841143d399693a40
mayudong200333 <mayudong200333@gmail.com>
2022-10-31 01:36:41 +0900

    create a new project

---
+++ contracts/Inbox.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.17;
+// linter warnings (red underline) about pragma version can igonored!
+
+// contract code will go here
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+
+    function getMessage() public view returns (string) {
+        return message;
+    }
+}
